56fc3b19fe0a8bf8ebc5100e2fed7c5179b205e2
Keegan Sequeira <keegan.sequeira@griffithuni.edu.au>
2023-09-23 15:14:28 +1000

    Updated signup to use mongo

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -1,55 +1,59 @@
-const fs = require("fs");
-
-module.exports = function(req, res) {
+module.exports = async(req, res) => {
     let email = req.body.email;
     let username = req.body.username;
     let password = req.body.password;
 
-    fs.readFile("./data/users.json", "utf8", function(err, data){
-        let existingJSON = JSON.parse(data);
-
-        // Check username exists
-        let existing = false;
-        for (let user of existingJSON){
-            if (user.username == username){
-                existing = true;
-                break;
-            }
-        }
-
-        if (existing == true){
-            res.send({valid: false});
-        } else {
-            let userID = existingJSON.length + 1;
-        
-            let newUser = {
-                "id": userID,
-                "username": username,
-                "password": password,
-                "email": email,
-                "roles": ["US"],
-                "groups": [1]
-            };
-        
-            existingJSON.push(newUser);
-        
-            let jsonString = JSON.stringify(existingJSON);
     
-            fs.writeFile("./data/users.json", jsonString, err => {
-                if (err){
-                    res.send({successful: false});
-                } else {
-                    let response = {
-                        valid: true,
-                        username: newUser.username,
-                        email: newUser.email,
-                        id: newUser.id,
-                        groups: newUser.groups,
-                        roles: newUser.roles
-                    };
-                    res.send(response);
+    const mongoClient = req.app.get("mongoClient");
+    const db = mongoClient.db("chatapp");
+    const collection = db.collection("users");
+
+    const existing = await collection.find({"username": username}).toArray();
+    if (existing.length > 0){
+        res.send({valid: false});
+    } else {
+        const topID = await collection.aggregate([
+            {
+                "$sort": {"id": -1}
+            },
+            {
+                "$limit": 1
+            },
+            {
+                "$project": {
+                    "_id": 0,
+                    "id": 1
                 }
-            });
+            }
+        ]).toArray();
+        const newID = topID[0].id + 1;
+        
+        let newUser = {
+            "id": newID,
+            "username": username,
+            "password": password,
+            "email": email,
+            "roles": ["US"],
+            "groups": [1]
         }
-    });
+
+        let response = {
+            valid: true,
+            username: newUser.username,
+            email: newUser.email,
+            id: newUser.id,
+            groups: newUser.groups,
+            roles: newUser.roles
+        };
+
+        const result = await collection.insertOne(newUser);
+        console.log(result);
+
+        if (result.acknowledged == true) {
+            res.send(response);
+        } else {
+            res.send({valid: false});
+        }
+        
+    }
 }
