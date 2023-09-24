02350e0a68f55beb5fcae73cc1f89ce3248a68a5
Keegan Sequeira <keegan.sequeira@griffithuni.edu.au>
2023-09-24 10:43:21 +1000

    Updated usergroups route to use mongo

--- server/routes/usergroups.js
+++ server/routes/usergroups.js
@@ -1,17 +1,11 @@
-const fs = require("fs");
-
-module.exports = function(req, res){
+module.exports = async(req, res) => {
     let groups = req.body.groups;
-    let returnGroups = [];
 
-    fs.readFile("./data/groups.json", "utf8", function(err, data){
-        let groupData = JSON.parse(data);
+    const mongoClient = req.app.get("mongoClient");
+    const db = mongoClient.db("chatapp");
+    const collection = db.collection("groups");
 
-        for (let group of groupData){
-            if (groups.includes(group.id)){
-                returnGroups.push(group);
-            }
-        }
-        res.send({groups: returnGroups});
-    });
+    const returnGroups = await collection.find({"id": {"$in": groups}}).toArray();
+
+    res.send({groups: returnGroups});
 };
