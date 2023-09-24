6e66395a547a88132ede09de0c0681553828243a
Keegan Sequeira <keegan.sequeira@griffithuni.edu.au>
2023-09-24 11:32:44 +1000

    Updated create and remove channel routes to use mongo

--- server/routes/deletechannel.js
+++ server/routes/deletechannel.js
@@ -1,32 +1,18 @@
-const fs = require("fs");
-
-module.exports = function(req, res){
+module.exports = async(req, res) => {
     let groupID = req.body.groupID;
     let channel = req.body.channel;
 
-    fs.readFile("./data/groups.json", "utf8", function(err, data){
-        let existingJson = JSON.parse(data);
-        let updateChannels = existingJson[groupID-1].channels;
-        let removeIndex;
+    const mongoClient = req.app.get("mongoClient");
+    const db = mongoClient.db("chatapp");
+    const collection = db.collection("groups");
 
-        for (let i = 0; i < updateChannels.length; i++){
-            if (updateChannels[i] == channel){
-                removeIndex = i;
-                break;
-            }
-        }
-        updateChannels.splice(removeIndex, 1);
-        existingJson[groupID-1].channels = updateChannels;
+    const result = await collection.updateOne({"id": groupID}, {"$pull": {"channels": channel}});
 
-        let jsonString = JSON.stringify(existingJson);
-
-        fs.writeFile("./data/groups.json", jsonString, err => {
-            if (err){
-                console.log(err);
-                res.send({successful: false});
-            } else {
-                res.send({successful: true});
-            }
-        });
-    });
+    
+    if (result.acknowledged == true)
+    {
+        res.send({successful: true});
+    } else {
+        res.send({successful: false});
+    }
 };
