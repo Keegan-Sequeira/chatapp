073d90198e0e24a567755e354885124e2b1ddf6c
Keegan-Sequeira <101374214+Keegan-Sequeira@users.noreply.github.com>
2023-10-11 21:01:11 +1000

    More e2e testing

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -26,36 +26,86 @@ describe('Test Login, Signup, and Logout Functions', () => {
     });
   })
 
-  it("should create new user from sign up", () => {
+  // it("should create new user from sign up", () => {
 
-    // Open sign up page
-    cy.get(".nav-link").contains("Signup").click();
+  //   // Open sign up page
+  //   cy.get(".nav-link").contains("Signup").click();
+
+  //   // Fill in the form
+  //   cy.get("#email").type("testing@chatlink.com");
+  //   cy.get("#username").type("test_user");
+  //   cy.get("#password").type("Password1");
+
+  //   // Submit the form
+  //   cy.get("#signup-button").click();
+
+  //   cy.location("pathname", {timeout: 60000}).should("equal", "/");
+
+  //   cy.window().then((win) => {
+  //     expect(localStorage.getItem("valid")).to.equal("true");
+  //     expect(localStorage.getItem("username")).to.equal("test_user");
+  //     expect(localStorage.getItem("email")).to.equal("testing@chatlink.com");
+  //   })
+  // })
+
+  // it("should log user out", () => {
+  //   cy.get(".nav-link").contains("Logout").click();
+
+  //   cy.location("pathname", {timeout: 60000}).should("equal", "/");
+
+  //   cy.window().then((win) => {
+  //     expect(localStorage.getItem("username")).to.equal(null);
+  //   })
+  // })
+
+  it("should log into the application", () => {
+    cy.get(".nav-link").contains("Login").click();
 
     // Fill in the form
-    cy.get("#email").type("testing@chatlink.com");
-    cy.get("#username").type("test_user");
-    cy.get("#password").type("Password1");
+    cy.get("#username").type("super");
+    cy.get("#password").type("123");
 
     // Submit the form
-    cy.get("#signup-button").click();
+    cy.get("#login-button").click();
 
     cy.location("pathname", {timeout: 60000}).should("equal", "/");
 
     cy.window().then((win) => {
       expect(localStorage.getItem("valid")).to.equal("true");
-      expect(localStorage.getItem("username")).to.equal("test_user");
-      expect(localStorage.getItem("email")).to.equal("testing@chatlink.com");
+      expect(localStorage.getItem("username")).to.equal("super");
+      expect(localStorage.getItem("email")).to.equal("admin@chatlink.com");
     })
   })
 
-  it("should log user out", () => {
-    console.log(localStorage.getItem("username"));
-    cy.get(".nav-link").contains("Logout").click();
+  // it("should create a new group", () => {
 
-    cy.location("pathname", {timeout: 60000}).should("equal", "/");
+  //   cy.get(".nav-link").contains("Manage Groups").click();
 
-    cy.window().then((win) => {
-      expect(localStorage.getItem("username")).to.equal(null);
-    })
+  //   cy.get("#name").type("End to End Group");
+  //   cy.get("button").contains("Create Group").click();
+
+  //   cy.wait(2000);
+  //   cy.get(".list-group-item").contains("End to End Group").should("exist");
+  // })
+
+  it("should add a new channel to Open Group", () => {
+    cy.get(".nav-link").contains("Manage Groups").click();
+    cy.get(".list-group-item").contains("Open Group").click();
+
+    cy.get("#name").type("Channel 4");
+    cy.get("button").contains("Create Channel").click();
+
+    cy.wait(2000);
+    cy.get(".list-group-item").contains("Channel 4").should("exist");
+  })
+
+  it("should delete Channel 4 from Open Group", () => {
+    cy.get(".nav-link").contains("Manage Groups").click();
+    cy.get(".list-group-item").contains("Open Group").click();
+
+    cy.get('.channel').contains('li', 'Channel 4').next('button').contains('Delete').click();
+
+    cy.wait(2000);
+    cy.get(".list-group-item").contains("Channel 4").should("not.exist");
   })
 })
